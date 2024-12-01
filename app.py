02bc46f9828600b791aa32f669d971acbcf10231
MirAliNaqiTalpur <antalpur@gmail.com>
2024-12-01 19:30:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -94,6 +94,6 @@ def predict_crop():
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
-if __name__ == '__main__':
-    app.run(debug=True, port = 8080)
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
